b1e0f888aa8bcce3b9154b0f8ae49417b9e66b75
Lucas Mariano <lucas.carmo@localiza.com>
2021-05-23 16:30:22 -0300

    Add project files.

---
+++ ServiceWorkerCronJobDemo/Services/Jobs/JobServ1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceWorkerCronJobDemo.Services
+{
+    public class JobServ1 : CronJobService
+    {
+        private readonly ILogger<JobServ1> _logger;
+
+        public JobServ1(IScheduleConfig<JobServ1> config, ILogger<JobServ1> logger)
+            : base(config.CronExpression, config.TimeZoneInfo)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 1 starts.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob 1 is working.");
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 1 is stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
---
+++ ServiceWorkerCronJobDemo/Services/Jobs/JobServ3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceWorkerCronJobDemo.Services
+{
+    public class JobServ3 : CronJobService
+    {
+        private readonly ILogger<JobServ3> _logger;
+
+        public JobServ3(IScheduleConfig<JobServ3> config, ILogger<JobServ3> logger)
+            : base(config.CronExpression, config.TimeZoneInfo)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 3 starts.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob 3 is working.");
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 3 is stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
